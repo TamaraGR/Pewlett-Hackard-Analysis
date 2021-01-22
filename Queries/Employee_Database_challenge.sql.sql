32e6e2a29a940749d6929396b3e7f7e9b704d45a
Tamara <grigoryeva.tamara@gmail.com>
2021-01-22 17:28:01 -0500

    committing change

--- Queries/Employee_Database_challenge.sql.sql
+++ Queries/Employee_Database_challenge.sql.sql
@@ -30,3 +30,26 @@ FROM unique_titles AS ut
 GROUP BY ut.title
 ORDER BY COUNT DESC; 
 
+-- DELIVERABLE 2, MENTORSHIP ELIGIBILITY PROGRAM
+
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no, 
+	e.first_name,
+	e.last_name, 
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship
+FROM employees AS e,
+LEFT JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+LEFT JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (birthdate BETWEEN '1965-01-01'	AND '1965-12-31')
+ORDER BY emp_no;
+	
+	
+	
+	
+	
